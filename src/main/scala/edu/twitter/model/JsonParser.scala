9297b4391d6d4daaf25a78c2a13bb32e2846818e
Mohamed Elhawaty <mo7daboelftouh@gmail.com>
2018-03-01 13:58:28 +0200

    New Neural Network model

---
+++ src/main/scala/edu/twitter/model/JsonParser.scala
@@ -0,0 +1,22 @@
+package edu.twitter.model
+
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Class responsible for parsing JSON files.
+  *
+  * @param sc: SparkContext used to read training data
+  */
+class JsonParser(sc: SparkContext){
+
+  /**
+    * parse Json files. load the data into Spark. Spark makes it easy to load JSON-formatted data into a dataframe
+    * @param path path to json files
+    * @return DataFrame represent json objects
+    */
+  def parse(path: String): DataFrame = {
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    sqlContext.read.json(path)
+  }
+}
