6b52d11fd63d3d5ba009b00a2ec043ecadfc8720
tarek.nawara <tarek.nawara@gmail.com>
2018-03-02 23:00:07 +0200

    added persistence to elasticsearch

--- src/main/scala/edu/twitter/evaluation/ModelEvaluator.scala
+++ src/main/scala/edu/twitter/evaluation/ModelEvaluator.scala
@@ -4,9 +4,20 @@ import edu.twitter.model.TweetsLoader
 import edu.twitter.model_api.GenericModel
 import org.apache.spark.SparkContext
 import org.apache.spark.mllib.feature.HashingTF
+import org.apache.spark.rdd.RDD
+import org.elasticsearch.spark.rdd.EsSpark
 
+/** Grouping of the evaluation necessary fields
+  * these fields are used for evaluating the training error. */
 case class EvaluationFields(happyCorrect: Int, happyTotal: Int, sadCorrect: Int, sadTotal: Int) {
 
+  /**
+    * Combining two evaluation instances.
+    *
+    * @param o instance to combine with this one.
+    * @return a new instance resulting of the combination of
+    *         both instances.
+    */
   def +(o: EvaluationFields): EvaluationFields = o match {
     case EvaluationFields(oHappyCorrect, oHappyTotal, oSadCorrect, oSadTotal) =>
       EvaluationFields(
@@ -18,22 +29,73 @@ case class EvaluationFields(happyCorrect: Int, happyTotal: Int, sadCorrect: Int,
   }
 }
 
+/** Representation of a training tweet that has been classified
+  * by the model, it holds both the actual and model labels. */
+case class EvaluatedTrainingTweet(actualLabel: Double, modelPrediction: Double, tweetText: String)
+
+/**
+  * Responsible for evaluating a model based on a given
+  * testing data. this class will print training analysis
+  * in the console and persist the labeled data in `Elasticsearch`
+  * for further visualization.
+  *
+  * @param sc spark context.
+  */
 class ModelEvaluator(sc: SparkContext) {
+  /**
+    * Given a model will evaluate it based on
+    * training data and log evaluation analysis.
+    *
+    * @param model target model for evaluation.
+    */
   def evaluate(model: GenericModel): Unit = {
+    val evaluation = evaluateData(model)
+    performEvaluationAnalysis(evaluation)
+  }
+
+  /**
+    * Given a model will evaluate it based on
+    * training data, log evaluation analysis and
+    * persist the labeled records in `Elasticsearch`
+    *
+    * @param model target model for evaluation
+    */
+  def evaluateAndPersist(model: GenericModel): Unit = {
+    val evaluation = evaluateData(model)
+    performEvaluationAnalysis(evaluation)
+    EsSpark.saveToEs(evaluation, "training-analysis")
+  }
+
+  /**
+    * Evaluate all the testing data.
+    *
+    * @param model target model for evaluation
+    * @return rdd of `EvaluatedTrainingTweet` instances.
+    */
+  private def evaluateData(model: GenericModel): RDD[EvaluatedTrainingTweet] = {
     val tweetsLoader = new TweetsLoader(sc)
     val hashingTF = new HashingTF(2000)
 
     val evaluation = for {
       row <- tweetsLoader.getTweetsDataSet()
       actualLabel = row.getAs[Double]("label")
-      tokens = row.getAs[String]("msg").split(" ").toSeq
+      tweetText = row.getAs[String]("msg")
+      tokens = tweetText.split(" ").toSeq
       features = hashingTF.transform(tokens)
       modelPrediction = model(features)
-    } yield (actualLabel, modelPrediction)
+    } yield EvaluatedTrainingTweet(actualLabel, modelPrediction, tweetText)
 
+    evaluation
+  }
 
+  /**
+    * Perform basic analysis over the model's classification.
+    *
+    * @param evaluation classified tweets
+    */
+  private def performEvaluationAnalysis(evaluation: RDD[EvaluatedTrainingTweet]): Unit = {
     val e = evaluation.aggregate(EvaluationFields(0, 0, 0, 0))(
-      (e, p) => p match {
+      (e, t) => (t.actualLabel, t.modelPrediction) match {
         case (1, 1) => e.copy(happyCorrect = e.happyCorrect + 1, happyTotal = e.happyTotal + 1)
         case (1, 0) => e.copy(happyTotal = e.happyTotal + 1)
         case (0, 1) => e.copy(sadTotal = e.sadTotal + 1)
@@ -47,9 +109,8 @@ class ModelEvaluator(sc: SparkContext) {
     println("sad % correct: " + e.sadCorrect.toDouble / e.sadTotal)
 
     val recordCount = evaluation.count()
-    var testErr = evaluation.filter(r => r._1 != r._2).count.toDouble / recordCount
+    val testErr = evaluation.filter(t => t.actualLabel != t.modelPrediction).count.toDouble / recordCount
     println("data size=" + recordCount)
     println("Test Error " + testErr)
   }
-
 }
