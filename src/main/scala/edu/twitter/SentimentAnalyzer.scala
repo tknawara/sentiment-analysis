66af3a02b02a79fbffab5bd71279ac4b30ba9525
tarek.nawara <tarek.nawara@gmail.com>
2018-03-09 23:06:21 +0200

    persist to different elasticsearch indicies for each model

--- src/main/scala/edu/twitter/SentimentAnalyzer.scala
+++ src/main/scala/edu/twitter/SentimentAnalyzer.scala
@@ -34,7 +34,7 @@ object SentimentAnalyzer extends App {
 
   val classifier = new Classifier(ssc)
   val classifiedStream = classifier.createClassifiedStream(GradientBoostingModel.name)
-  classifiedStream.foreachRDD(EsSpark.saveToEs(_, "twitter/sentiment"))
+  classifiedStream.foreachRDD(EsSpark.saveToEs(_, s"${GradientBoostingModel.name}/classified-stream"))
 
   ssc.start()
   ssc.awaitTermination()
