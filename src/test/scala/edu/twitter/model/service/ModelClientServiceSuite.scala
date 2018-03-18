625c3dddcb8bde6074335450375d33aee993f202
tarek.nawara <tarek.nawara@gmail.com>
2018-03-19 00:26:20 +0200

    fix build

--- src/test/scala/edu/twitter/model/service/ModelClientServiceSuite.scala
+++ src/test/scala/edu/twitter/model/service/ModelClientServiceSuite.scala
@@ -8,15 +8,16 @@ import org.scalatest.{BeforeAndAfterAll, FunSuite}
 @RunWith(classOf[JUnitRunner])
 class ModelClientServiceSuite extends FunSuite with BeforeAndAfterAll {
   @transient private var modelService: ModelService = _
+  private val builder = new TestGenericModelBuilder(0, "ModelOne")
 
   override def beforeAll(): Unit = {
-    modelService = new ModelService(List(new TestGenericModelBuilder()))
+    modelService = new ModelService(List(builder))
     modelService.start()
   }
 
   test("the model client's result should match the model's response") {
-    val resp = ModelClient.callModelService(TestGenericModel.name, "hello")
-    assert(resp.get().getLabel == TestGenericModel.fixedLabel)
+    val resp = ModelClient.callModelService(builder.modelName, "hello")
+    assert(resp.get().getLabel == builder.fixedLabel)
   }
 
   override def afterAll(): Unit = {
