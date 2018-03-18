ecede81dc8363a7853310b3972b4d1fb9624e1a9
tarek.nawara <tarek.nawara@gmail.com>
2018-03-19 00:40:16 +0200

    added documentation

--- src/main/scala/edu/twitter/model/impl/TweetTextFilter.scala
+++ src/main/scala/edu/twitter/model/impl/TweetTextFilter.scala
@@ -1,8 +1,18 @@
 package edu.twitter.model.impl
 
+/** Responsible for filtering the tweet
+  * text and removing tokens that could affect
+  * the classification accuracy.
+  */
 object TweetTextFilter {
   private val invalidTokens = Set("http", "@", "#")
 
+  /**
+    * Remove all invalid tokens from tweet.
+    *
+    * @param tweet tweet text
+    * @return filtered tweet
+    */
   def filterTweet(tweet: String): String = {
     tweet.split(" ").filter(s => invalidTokens.forall(!s.contains(_))).mkString(" ")
   }
