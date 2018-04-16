c95253c60f934af041d2b5147b2f28c9cb9017e2
tarek-nawara <tarek.nawara@gmail.com>
2018-04-17 00:29:37 +0200

    added test for the failed tweet

---
+++ src/test/scala/edu/twitter/model/client/ModelClientFailedTweetSuite.scala
@@ -0,0 +1,43 @@
+package edu.twitter.model.client
+
+import edu.twitter.config.{AppConfig, DevConfig}
+import edu.twitter.model.Label
+import edu.twitter.model.impl.gradientboosting.{GradientBoostingBuilder, GradientBoostingModel}
+import edu.twitter.model.impl.neuralnetwork.{NeuralNetworkBuilder, NeuralNetworkModel}
+import edu.twitter.model.service.ModelService
+import org.apache.spark.{SparkConf, SparkContext}
+import org.junit.runner.RunWith
+import org.scalatest.{BeforeAndAfterAll, FunSuite, Ignore}
+import org.scalatest.junit.JUnitRunner
+
+@Ignore
+@RunWith(classOf[JUnitRunner])
+class ModelClientFailedTweetSuite extends FunSuite with BeforeAndAfterAll {
+  @transient private var sc: SparkContext = _
+  @transient private var modelService: ModelService = _
+  implicit val appConfig: AppConfig = DevConfig
+
+  override def beforeAll(): Unit = {
+    val conf = new SparkConf().setMaster("local[*]").setAppName("ModelEvaluatorTest")
+      .set("spark.driver.allowMultipleContexts", "true")
+    sc = new SparkContext(conf)
+    modelService = new ModelService(List(new NeuralNetworkBuilder(sc), new GradientBoostingBuilder(sc)))
+    modelService.start()
+  }
+
+  test("test jackson") {
+    val text = "RT @ChildhoodShows: Before there was Troy Bolton there was Eddie Thomas https://t.co/QlxfSvtojc"
+    val resOne = ModelClient.callModelService(NeuralNetworkModel.name, text)
+    assert(resOne.get == Label.HAPPY)
+
+    val resTwo = ModelClient.callModelService(GradientBoostingModel.name, text)
+    assert(resTwo.nonEmpty)
+  }
+
+  override def afterAll(): Unit = {
+    if (sc != null) sc.stop()
+    if (modelService != null) {
+      modelService.stop()
+    }
+  }
+}
