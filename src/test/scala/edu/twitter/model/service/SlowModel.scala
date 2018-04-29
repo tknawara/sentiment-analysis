b494e719a14466ecd9f9c9e120cd77d76d798a30
tarek-nawara <tarek.nawara@gmail.com>
2018-04-29 22:34:09 +0200

    added timeout in model client

---
+++ src/test/scala/edu/twitter/model/service/SlowModel.scala
@@ -0,0 +1,23 @@
+package edu.twitter.model.service
+
+import edu.twitter.model.Label
+import edu.twitter.model.api.GenericModel
+
+class SlowModel(sleepInterval: Long) extends GenericModel {
+  override def name: String = SlowModel.name
+
+  /**
+    * Classify the given tweet.
+    *
+    * @param tweetText target tweet message for classification.
+    * @return 0 for sad & 1 for happy
+    */
+  override def getLabel(tweetText: String): Label = {
+    Thread.sleep(sleepInterval)
+    Label.HAPPY
+  }
+}
+
+object SlowModel {
+  val name: String = "SlowModel"
+}
