6afc3227d1d6b44bc54c2f3ed36229fb6cf45600
Sherif Hamdy <sherif.hamdy.1995@gmail.com>
2018-05-01 17:53:50 +0200

    refactorin

--- src/main/java/edu/twitter/service/SpellingCorrectionService.java
+++ src/main/java/edu/twitter/service/SpellingCorrectionService.java
@@ -2,13 +2,11 @@ package edu.twitter.service;
 
 import org.languagetool.JLanguageTool;
 import org.languagetool.language.AmericanEnglish;
-import org.languagetool.rules.RuleMatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scala.Option;
 
 import java.io.IOException;
-import java.util.List;
 
 /**
  * Spelling Correction service used to correct spelling mistakes in tweet message.
@@ -32,15 +30,15 @@ public final class SpellingCorrectionService {
      * @param tweetMessage to be corrected.
      * @return tweetMessage after correcting spelling.
      */
-    public static Option<String> correctSpelling(String tweetMessage) {
+    public static Option<String> correctSpelling(final String tweetMessage) {
         try {
-            List<RuleMatch> matches = langTool.check(tweetMessage);
-            StringBuilder tweetStringBuilder = new StringBuilder(tweetMessage);
-            for (RuleMatch match : matches) {
-                if (!match.getSuggestedReplacements().isEmpty()) {
-                    tweetStringBuilder.replace(match.getFromPos(), match.getToPos(), match.getSuggestedReplacements().get(0));
-                }
-            }
+            final StringBuilder tweetStringBuilder = new StringBuilder(tweetMessage);
+            langTool.check(tweetMessage)
+                    .stream()
+                    .filter(matcher -> !matcher.getSuggestedReplacements().isEmpty())
+                    .forEach(matcher ->
+                            tweetStringBuilder.replace(matcher.getFromPos(),
+                                    matcher.getToPos(), matcher.getSuggestedReplacements().get(0)));
             return Option.apply(tweetStringBuilder.toString());
         } catch (IOException e) {
             LOGGER.warn("error in correcting tweet with message {} {}", tweetMessage, e.getMessage());
