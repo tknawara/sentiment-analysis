73e86ee4421284de68e6cfdc2e8ce37c8faf2ead
Mohamed Elhawaty <mo7daboelftouh@gmail.com>
2018-05-03 01:06:32 +0200

    Fix spelling correction bug

--- src/main/java/edu/twitter/service/SpellingCorrectionService.java
+++ src/main/java/edu/twitter/service/SpellingCorrectionService.java
@@ -2,8 +2,13 @@ package edu.twitter.service;
 
 import org.languagetool.JLanguageTool;
 import org.languagetool.language.AmericanEnglish;
+import org.languagetool.rules.RuleMatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Spelling Correction service used to correct spelling mistakes in tweet message.
@@ -26,14 +31,24 @@ public final class SpellingCorrectionService {
      */
     public static String correctSpelling(final String tweetMessage) {
         try {
-            final StringBuilder tweetStringBuilder = new StringBuilder(tweetMessage);
+            final StringBuilder tweetStringBuilder = new StringBuilder();
             final JLanguageTool langTool = new JLanguageTool(new AmericanEnglish());
-            langTool.check(tweetMessage)
+            final List<RuleMatch> matches = langTool.check(tweetMessage)
                     .stream()
-                    .filter(matcher -> !matcher.getSuggestedReplacements().isEmpty())
-                    .forEach(matcher ->
-                            tweetStringBuilder.replace(matcher.getFromPos(),
-                                    matcher.getToPos(), matcher.getSuggestedReplacements().get(0)));
+                    .filter(l -> !l.getSuggestedReplacements().isEmpty())
+                    .sorted(Comparator.comparingInt(RuleMatch::getFromPos))
+                    .collect(Collectors.toList());
+            int idx = 0;
+            for (int i = 0; i < tweetMessage.length(); ) {
+                if (idx < matches.size() && i == matches.get(idx).getFromPos()) {
+                    i = matches.get(idx).getToPos() + 1;
+                    tweetStringBuilder.append(matches.get(idx).getSuggestedReplacements().get(0));
+                    ++idx;
+                } else {
+                    tweetStringBuilder.append(tweetMessage.charAt(i));
+                    ++i;
+                }
+            }
             return tweetStringBuilder.toString();
         } catch (final Exception e) {
             LOGGER.warn("error in correcting tweet with message {} {}", tweetMessage, e.getMessage());
