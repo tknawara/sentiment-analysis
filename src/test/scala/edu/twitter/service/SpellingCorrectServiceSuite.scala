817a4b492bf88eac1d7077659010ea1b5691b9a9
tarek-nawara <tarek.nawara@gmail.com>
2018-05-02 17:49:44 +0200

    fix test

--- src/test/scala/edu/twitter/service/SpellingCorrectServiceSuite.scala
+++ src/test/scala/edu/twitter/service/SpellingCorrectServiceSuite.scala
@@ -10,8 +10,7 @@ class SpellingCorrectServiceSuite extends FunSuite {
   test("should correct spelling mistakes in tweet") {
     val tweetMessage = "I havv a goood spelling"
     val correctSpelling = SpellingCorrectionService.correctSpelling(tweetMessage)
-    assert(correctSpelling.nonEmpty)
     val correct = "I have a good spelling"
-    assert(correctSpelling.get == correct)
+    assert(correctSpelling == correct)
   }
 }
